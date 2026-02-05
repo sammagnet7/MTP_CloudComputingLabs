f0238d46526fde6779ee9fb0f0325dd94dedeb93
Soumik Dutta <sammagnet7@gmail.com>
2026-02-05 14:57:37 +0530

    Adds initial folder structure for Microservices lab for MTP2

---
+++ Migration_to_Micro-Services_lab/Codebase/eCommerce-Monolith/src/main/java/com/cse/bombay/iit/eCommerce_Monolith/ECommerceMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.cse.bombay.iit.eCommerce_Monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMonolithApplication.class, args);
+	}
+
+}
